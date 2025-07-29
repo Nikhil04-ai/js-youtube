e75e118ea67069b39ed1493ce913cffbd1219dc7
Nikhil04-ai <nikhilmehta1323@gmail.com>
2025-07-29 10:33:47 +0000

    conversion of Operation 01

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -25,4 +25,32 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) 
+
+// ******************* Operations *********************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+let str1 = "hello"
+let str2 =" Nikhil"
+
+let str3 =str1 + str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log("1"+2+2);
+console.log(1+2+"2");   //32 not 122
+
+console.log(+true); //1
+console.log(+"");   //0
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
