252adb9704341568940286d998e7c4f37f125038
Nikhil04-ai <nikhilmehta1323@gmail.com>
2025-08-02 19:24:19 +0000

    function1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("K");
+    console.log("H");
+    console.log("I");
+    console.log("L");
+}
+
+sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(3,5)
+console.log("result:",result);
