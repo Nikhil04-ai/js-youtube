101f2881bbe4e55d67c59b06902d7d75777ae79f
Nikhil04-ai <nikhilmehta1323@gmail.com>
2025-08-03 10:01:01 +0000

    functions with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,27 @@ function addTwoNumbers(number1, number2){
 }
 const result = addTwoNumbers(3,5)
 console.log("result:",result);
+
+function calculateCartPrice(val1,val2, ...num1){   // rest operator
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000))
+ 
+const user ={
+    username: "Nikhil",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
+}
+
+handleObject(user)
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
